51612f8682f0a8faa050b9d646d53d9330658244
Levi Giraud <levigiraud@Levis-MacBook-Air.local>
2020-04-24 19:48:04 -0600

    added some components and created the file structure of the app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Layout.module.css";
+
+const Layout = (props) => (
+  <>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </>
+);
+
+export default Layout;
