a9a699a1bfcbafd8e751eaf53fcfe784a53f5628
Levi Giraud <levigiraud@Levis-MacBook-Air.local>
2020-05-06 21:06:01 -0600

    streamlined unnecessary rerenders and finished the basic app

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,39 @@
 import React from "react";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((key) => {
+class OrderSummary extends React.Component {
+  //This could be a functional component if needed
+  componentDidUpdate() {
+    console.log("[OrderSummary] WillUpdate");
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((key) => {
+      return (
+        <li key={key}>
+          <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
+          {this.props.ingredients[key]}
+        </li>
+      );
+    });
     return (
-      <li key={key}>
-        <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
-        {props.ingredients[key]}
-      </li>
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </>
     );
-  });
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        Continue
-      </Button>
-    </>
-  );
-};
+  }
+}
 
 export default OrderSummary;
